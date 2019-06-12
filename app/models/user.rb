b55900d632e1d1b58c979e6d1206f5d0069e77ef
martynj2001 <martynj2010@icloud.com>
2019-06-12 20:59:39 +0100

    Continued Devise authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :set_admin
+
+  private
+  def set_admin
+    if User.count == 1
+      User.first.update_attribute(:admin, true)
+    else
+      return true
+    end
+  end
+  
 end
